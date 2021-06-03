5b7bc2b8e83055e6c930268b537aafec0159ef89
powellgatwit <powellg@wit.edu>
2021-06-03 19:44:31 -0400

    Styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,22 @@ var h = d.getHours();
 //between sunrise and 12pm, morning
 
 function greeting() {
-    //between sunrise and 12pm, morning
-    if (h > 7 && h < 12) {
+    if (h >= 6 && h < 12) {
+        //between 6am and 12pm, morning
         document.getElementById("mor").style.display = "inline";
+        document.body.style.backgroundColor = "#114C4B";
     } else if (h >= 12 && h < 19) {
+        //between 12pm and 7pm, afternoon
         document.getElementById("aft").style.display = "inline";
-    } else if (h >= 19 && h < 24) {
+        document.body.style.backgroundColor = "#50351D";
+    } else if (h >= 19 && h < 22) {
+        //between 7pm and 10pm, evening
         document.getElementById("eve").style.display = "inline";
+        document.body.style.backgroundColor = "#291D50";
     } else {
+        //else (10pm and 6am), night
         document.getElementById("nig").style.display = "inline";
+        document.body.style.backgroundColor = "#1D1D1D";
     }
 }
 
