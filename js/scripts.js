1aa173ff01652de17eb0e6f688f045499a2d3f82
powellgatwit <powellg@wit.edu>
2021-06-23 12:16:23 -0400

    Created second page, tweaked styling

--- js/scripts.js
+++ js/scripts.js
@@ -24,10 +24,10 @@ function greeting() {
 function bgroundcolor() {
     if (h >= 6 && h < 12) {
         //between 6am and 12pm, morning
-        document.body.style.backgroundColor = "#005967";
+        document.body.style.backgroundColor = "#004853";
     } else if (h >= 12 && h < 19) {
         //between 12pm and 7pm, afternoon
-        document.body.style.backgroundColor = "#50351D";
+        document.body.style.backgroundColor = "#003A25";
     } else if (h >= 19 && h < 22) {
         //between 7pm and 10pm, evening
         document.body.style.backgroundColor = "#291D50";
@@ -48,11 +48,45 @@ function copydate() {
     }
 }
 
-//functions to run on page load
+//Typewriter JS by Geoff Graham
+// set up text to print, each item in array is new line
+var aText = new Array("George Powell");
+var iSpeed = 65; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+     
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+     
+function typewriter() {
+    sContents =  ' ';
+    iRow = Math.max(0, iIndex-iScrollAt);
+    var destination = document.getElementById("name");
+     
+    while ( iRow < iIndex ) {
+        sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
+    if ( iTextPos++ == iArrLength ) {
+        iTextPos = 0;
+        iIndex++;
+        if ( iIndex != aText.length ) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+        }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+//functions to run on page load (all pages)
 function run() {
-    greeting();
     bgroundcolor();
     copydate();
 }
-
 window.onload = run();
+
+//greeting() must be run on the custom homepage
+//typewriter() must be run on index.html
