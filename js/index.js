6195541b1139c5c0e5e6c957ff2f154dbbe01c29
george <powellg@wit.edu>
2022-01-25 12:59:57 -0500

    Refreshed information and format

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ function menu(page, id) {
     document.getElementById("about").style.display = "none";
     document.getElementById("contact").style.display = "none";
     document.getElementById("resume").style.display = "none";
-    document.getElementById("landing").style.display = "none";
+    document.getElementById("home").style.display = "none";
     //no more underlines
     document.getElementById("a").style.textDecorationColor = "transparent";
     document.getElementById("b").style.textDecorationColor = "transparent";
     document.getElementById("c").style.textDecorationColor = "transparent";
+    document.getElementById("h").style.textDecorationColor = "transparent";
     //show selected content
     document.getElementById(page).style.display = "inline";
     //underline the selected page
@@ -26,16 +27,16 @@ var iSpeed = 55; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
-     
+
 var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
-     
+
 function typewriter() {
     sContents =  ' ';
     iRow = Math.max(0, iIndex-iScrollAt);
     var destination = document.getElementById("name");
-     
+
     while ( iRow < iIndex ) {
         sContents += aText[iRow++] + '<br />';
     }
